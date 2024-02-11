cc84bdcc59c42c55677a9e0de2926e4332a8e75b
Phillip <phillip.a.jordan@gmail.com>
2024-02-11 09:55:34 -0500

    Pushing changes before trying ssl on server side

--- src/App.tsx
+++ src/App.tsx
@@ -6,26 +6,20 @@ function App() {
   return (
     <div className="App">
       <div className="hero h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content flex-col lg:flex-row">
           <img src="img/pfp.jpg" className="max-w-sm rounded-lg shadow-2x1" />
           <div className="text-left w-2/5">
             <h1 className="text-5xl font-bold">Phillip Jordan</h1>
-            <p className="py-6">I'm a Virginia Tech student and cadet majoring in <span className="text-sky-400">computer science</span>, looking for a career in software engineering and development on a global scale.</p>
-            <p className="py-10">Lorem ipsum</p>
+            <p className="py-6">I'm a Virginia Tech student and cadet majoring in <span className="text-sky-400">Computer Science</span>, looking for a career in software engineering and development on a global scale.</p>
           </div>
         </div>
       </div>
-      <div className="navbar bg-base-300 text-xs">
-        <div className="navbar-start">
-          <a className="btn btn-ghost text-xl">Phillip</a>
-        </div>
-        <div className="navbar-center hidden lg:flex border-solid border-2 rounded-full border-sky-400 p-0 bg-base-200">
-          <ul className="menu menu-horizontal p-0">
-            <li><a className="rounded-full p-3 active"><img src="/img/home.png" className="w-5" /></a></li>
-            <li><a className="rounded-full p-3">Drones</a></li>
-            <li><a className="rounded-full p-3">Contact me</a></li>
+      <div className="navbar-center bg-base-300 text-xs p-2">
+          <ul className="menu menu-horizontal p-0 border-solid border-2 rounded-full border-sky-400 p-0 bg-base-200">
+            <li><a className="rounded-full p-3 active px-8">Home</a></li>
+            <li><a className="rounded-full p-3 px-8">Skills</a></li>
+            <li><a className="rounded-full p-3 px-8">Projects</a></li>
           </ul>
-        </div>
       </div>
     </div>
   );
