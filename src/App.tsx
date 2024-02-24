cf4ba2fa4e0d39e9df984ef7d106e6491b540fb4
Phillip <phillip.a.jordan@gmail.com>
2024-02-23 23:26:12 -0500

    Added basic loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,20 @@ class App extends React.Component {
   state = {
     loading: true
   };
-  
+
   componentDidMount() {
-    this.setState({loading: false});
+    this.setState({ loading: false });
   }
 
   render() {
-    const {loading} = this.state;
-    
-    if (loading){
-      return null;
+    const { loading } = this.state;
+
+    if (loading) {
+      return (
+        <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen overflow-hidden bg-zinc-900 flex flex-col items-center justify-center">
+          <div className="inline-block h-12 w-12 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status"></div>
+        </div>
+      );
     }
 
     return (
